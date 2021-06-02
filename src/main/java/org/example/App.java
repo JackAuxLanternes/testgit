a20071744b2fceca4cff21b50833d947fdd6c545
QuentinR <rouvilleq@gmail.com>
2021-06-02 09:50:22 +0200

    [6] test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        //
         Calculatrice calculatrice = new Calculatrice();
 
         System.out.println(calculatrice.addition(1,2));
